26b641b9e70da8c72f62c73677b5ff9f6813735b
rgermain <germain.remi@protonmail.com>
2020-08-31 17:19:58 +0200

    fix name function

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import babel from "@rollup/plugin-babel"
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: "src/index.js",
-    output: {
-        dir: "dist",
-        name: "ElementInView",
-        format: "umd"
-    },
-    plugins: [
-        babel({
-            babelHelpers: "bundled",
-            exclude: ["node_modules/**"]
-        }),
-        terser()
-    ]
-}
+  input: "src/index.js",
+  output: {
+    dir: "dist",
+    name: "elementInView",
+    format: "umd",
+  },
+  plugins: [
+    babel({
+      babelHelpers: "bundled",
+      exclude: ["node_modules/**"],
+    }),
+    terser(),
+  ],
+};
