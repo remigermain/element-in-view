41a607feebee95e78673d348078399620e115732
rgermain <germain.remi@protonmail.com>
2020-09-05 16:33:25 +0200

    fix offset top and left

--- src/index.js
+++ src/index.js
@@ -10,23 +10,23 @@ function __inView(el, options) {
   const elRect = el.getBoundingClientRect();
 
   /* if parent is not define , get the document element */
-  const parentRect = (options.parent || document.documentElement).getBoundingClientRect();
+  const parent = (options.parent || document.documentElement).getBoundingClientRect();
 
   /* construct with offset add or none */
-  const p = {
-    left: parentRect.left + (options.offsetX || options.offsetLeft || 0),
-    right: parentRect.right - (options.offsetX || options.offsetRight || 0),
-    top: parentRect.top + (options.offsetY || options.offsetTop || 0),
-    bottom: parentRect.bottom - (options.offsetY || options.offsetBottom || 0)
+  const target = {
+    left: elRect.left - (options.offsetX || options.offsetLeft || 0),
+    right: elRect.right + (options.offsetX || options.offsetRight || 0),
+    top: elRect.top - (options.offsetY || options.offsetTop || 0),
+    bottom: elRect.bottom + (options.offsetY || options.offsetBottom || 0)
   }
 
   /* for x axis */
-  const AxisRight = (p.left <= elRect.right && p.right > elRect.right)
-  const AxisLeft = (p.left <= elRect.left && p.right >= elRect.left)
+  const AxisRight = (parent.left <= target.right && parent.right >= target.right)
+  const AxisLeft = (parent.left <= target.left && parent.right >= target.left)
 
   /* for y axis */
-  const AxisBottom  = (p.top <= elRect.bottom && p.bottom > elRect.bottom)
-  const AxisTop  = (p.top <= elRect.top && p.bottom > elRect.top)
+  const AxisBottom  = (parent.top <= target.bottom && parent.bottom >= target.bottom)
+  const AxisTop  = (parent.top <= target.top && parent.bottom >= target.top)
 
   /* if partial need to
   ** top or bottom and right and left
